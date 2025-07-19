d2db4c81cc36c7533e2fe1c224a9f4c4bef39806
bigtaoge <115353710+bigtaoge@users.noreply.github.com>
2025-07-19 08:53:32 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MedicalImagingResearchTool from './components/MedicalImagingResearchTool';
+
+function App() {
+  return (
+    <div className="App">
+      <MedicalImagingResearchTool />
+    </div>
+  );
+}
+
+export default App;
